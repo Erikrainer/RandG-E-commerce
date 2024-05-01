ba3726f49344709eaed380c4ccd0bc42d2fd75f5
Erik <rainer.erik70@gmail.com>
2024-04-30 19:39:56 -0700

    index.js filled with all the has and belongs

--- models/index.js
+++ models/index.js
@@ -5,7 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product , {
+  foreignKey: "category_id"
+});
 
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: "tag_id"
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id"
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id"
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
